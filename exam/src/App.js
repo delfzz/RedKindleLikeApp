6ece54192ddc95102966604e14541eadf8379388
delfindanas69 <danasdelfin@gmail.com>
2021-11-03 14:25:57 +0800

    done

--- exam/src/App.js
+++ exam/src/App.js
@@ -2,11 +2,13 @@ import './App.css';
 import Files from './components/Files'
 
 function App() {
-  
+
   return (
     <div className = "App">
       <div>
+        
         <Files/>
+
       </div>
     </div>
   );
